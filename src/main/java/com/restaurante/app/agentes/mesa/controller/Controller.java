f15a0250ad19dc3e903c609def7808b2ae9c07e5
jisellExe <50428407+jisellExe@users.noreply.github.com>
2019-11-22 22:05:43 -0500

    Add files via upload

--- src/main/java/com/restaurante/app/agentes/mesa/controller/Controller.java
+++ src/main/java/com/restaurante/app/agentes/mesa/controller/Controller.java
@@ -4,11 +4,8 @@ import com.restaurante.app.agentes.mesa.model.DaoMesa;
 import com.restaurante.app.agentes.mesa.model.Mesa;
 
 public class Controller {
-	private DaoMesa daoMesa;
 	
-	public Controller() {
-		daoMesa = new DaoMesa();
-	}
+	private DaoMesa daoMesa;
 	
 	public Mesa obtenerMesaLibre(int numeroClientes) {
 		return daoMesa.getMesaLibre(numeroClientes);
@@ -17,4 +14,21 @@ public class Controller {
 	public void liberarMesa(int idMesa) {
 		daoMesa.liberarMesa(idMesa);
 	}
+//patron singleton------------------------------
+	
+	public static Controller instance;
+
+	//metodo que permite obtener una instancia de la clase
+	public static Controller gestInstance() {
+		if (instance == null) {
+			instance = new Controller();
+		}
+		return instance;
+	}
+	
+	//controlador privado para que no pueda ser accedido desde el exterior
+	private Controller() {
+		daoMesa = new DaoMesa();
+	}
 }
+
