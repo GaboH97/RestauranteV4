7527be2ad8b4ced879ecc2e79d89f850bb744425
Cesar Cardozo <srfeudalwow@gmail.com>
2020-02-02 12:53:00 -0500

    documentacion maquina del tiempo

--- src/main/java/maquinaDelTiempo/MaquinaDeltiempo.java
+++ src/main/java/maquinaDelTiempo/MaquinaDeltiempo.java
@@ -4,19 +4,42 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+
+/**
+ * clase master de los registros de instantes de tiempo en donde se consignaran cuando ocurren los cambios de estados de todos 
+ * los agentes 
+ * @author Cesar
+ *
+ */
 public class MaquinaDeltiempo {
 
-	private float relojGlobalAux = 0; //este se usara para ir creando los registros de la maquina del tiempo
+//	private float relojGlobalAux = 0; //este se usara para ir creando los registros de la maquina del tiempo
 	private long presente;//este servira para ir iterando los instantes de tiempo
 	private ArrayList<RegistroMaquinaDelTiempo> registros; //todos los registros
 	public static final Long TAMANO_INTERVALO = 1L; //el intervalo en el cual se graficaran los agentes
 	
 	
+	/**
+	 * metodo que ordena los registros de instantes de tiempo segun el orden de aparicion
+	 */
 	public void ordenarMaquinaDelTiempo() {
 		//se ordena toda la maquina del tiempo deacuerdo a los instantes de tiempo en los que ocurrieron las cosas 
 		Collections.sort(registros, Comparator.comparing(RegistroMaquinaDelTiempo::getInstanteTiempoInicio));
 	}
 	
+	/**
+	 * metodo que agrega un registro de instante de tiempo 
+	 */
+	public void agregarRegistro(Identificadores identificador, float instanteTiempoInicio,
+			float duracion, Object estadoDelAgente) {
+		this.registros.add(new RegistroMaquinaDelTiempo(identificador, instanteTiempoInicio, duracion, estadoDelAgente));
+	}
+	
+	/**
+	 * Cada vez que se llame este metodo se obtendran los datos que hay que pintar y se avanzara el reloj
+	 * al siguiente intervalo de tiempo para cuando se vuelva a llamar...
+	 * @return
+	 */
 	public ArrayList<RegistroMaquinaDelTiempo> avanzarIntervaloDeTiempo() {
 		ArrayList<RegistroMaquinaDelTiempo> registrosEnElIntervalo = new ArrayList<>();
 		for (RegistroMaquinaDelTiempo registro : registros) {
@@ -27,5 +50,4 @@ public class MaquinaDeltiempo {
 		presente+=TAMANO_INTERVALO;
 		return registrosEnElIntervalo;
 	}
-	
 }
--- src/main/java/maquinaDelTiempo/RegistroMaquinaDelTiempo.java
+++ src/main/java/maquinaDelTiempo/RegistroMaquinaDelTiempo.java
@@ -1,18 +1,24 @@
 package maquinaDelTiempo;
 
+/**
+ * Clase que determina los registros de la maquina del tiempo en los cuales se determina
+ * que tipo de agente se va a pintar, por cuanto tiempo y el estado del agente.
+ * @author Cesar
+ *
+ */
 public class RegistroMaquinaDelTiempo {
 	
-	private Identificadores identificador;
-	private float instanteTiempoInicio;
-	private float instanteTiempoFin;
-	private long  idMesa;
+	private Identificadores identificador; //servira para saber identificar el tipo de agente 
+	private float instanteTiempoInicio; //establece cuando se empezara a pintar el agente
+	private float instanteTiempoFin;//establece cuando se dejara de pintar el agente
+	private Object estadoDelAgente;//establece el estado del agente, esto para saber como debe ser pintado si es que tiene que ser pintado
 	
 	public RegistroMaquinaDelTiempo(Identificadores identificador, float instanteTiempoInicio,
-			float duracion) {
-		super();
+			float duracion, Object estadoDelAgente) {
 		this.identificador = identificador;
 		this.instanteTiempoInicio = instanteTiempoInicio;
 		this.instanteTiempoFin = this.instanteTiempoInicio + duracion;
+		this.estadoDelAgente = estadoDelAgente;
 	}
 	
 	public Identificadores getIdentificador() {
@@ -32,5 +38,13 @@ public class RegistroMaquinaDelTiempo {
 	}
 	public void setInstanteTiempoFin(float instanteTiempoFin) {
 		this.instanteTiempoFin = instanteTiempoFin;
+	}
+
+	public Object getEstadoDelAgente() {
+		return estadoDelAgente;
+	}
+
+	public void setEstadoDelAgente(Object estadoDelAgente) {
+		this.estadoDelAgente = estadoDelAgente;
 	}
 }
